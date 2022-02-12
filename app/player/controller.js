76b4907d6c7a9da1de8f24aa7c3456097e0e54d5
AlvinSanudharma <alvinsanudharma@gmail.com>
2022-02-12 18:10:24 +0800

    Api Detail History Transaksi

--- app/player/controller.js
+++ app/player/controller.js
@@ -3,6 +3,7 @@ const Category = require('../category/model');
 const Nominal = require('../nominal/model');
 const Payment = require('../payment/model');
 const Bank = require('../bank/model');
+const Transaction = require('../transaction/model');
 
 module.exports = {
     landingPage: async (req, res) => {
@@ -52,6 +53,7 @@ module.exports = {
                 .select('name category _id thumbnail user')
                 .populate('category')
                 .populate('user');
+                
             if(!res_voucher) return res.status(404).json({message: 'Voucher Not Found!'})
 
             const res_nominal = await Nominal.findOne({_id: nominal})
@@ -62,6 +64,100 @@ module.exports = {
 
             const res_bank = await Bank.findOne({_id: bank})
             if(!res_bank) return res.status(404).json({message: 'Bank Not Found!'})
+            // console.log(res_bank)
+            const tax = (10 / 100) * res_nominal._doc.price;
+            const value = res_nominal._doc.price - tax;
+            const payload = {
+                historyVoucherTopup: {
+                    gameName: res_voucher._doc.name,
+                    category: res_voucher._doc.category ? res_voucher._doc.category.name : '',
+                    thumbnail: res_voucher._doc.thumbnail,
+                    coinName: res_nominal._doc.coinName,
+                    coinQuantity: res_nominal._doc.coinQuantity,
+                    price: res_nominal._doc.price
+                },
+                historyPayment: {
+                    name: res_bank._doc.name,
+                    type: res_payment._doc.type,
+                    bankName: res_bank._doc.bankName,
+                    noRekening: res_bank._doc.noRekening,
+                },
+                name: name, 
+                accountUser: accountUser,
+                tax: tax,
+                value: value,
+                player: req.player._id,
+                historyUser: {
+                    name: res_voucher._doc.user?.name,
+                    phoneNumber: res_voucher._doc.user?.phoneNumber
+                },
+                category: res_voucher._doc.category?._id,
+                user: res_voucher._doc.user?._id
+            }
+
+            const transaction = new Transaction(payload);
+            await transaction.save()
+
+            res.status(201).json({
+                data: transaction
+            })
+        } catch (error) {
+            res.status(500).json({message: error.message || 'Internal Server Error!'});
+        }
+    },
+    history: async (req, res) => {
+        try {
+            const {status = ''} = req.query;
+
+            let criteria = {};
+
+            if (status.length > 1) {
+                criteria = {
+                    ...criteria,
+                    status: {$regex : `${status}`, $options: 'i'}
+                }
+            }
+
+            if (req.player._id) {
+                criteria = {
+                    ...criteria,
+                    player: req.player._id
+                }
+            }
+
+            const history = await Transaction.find(criteria);
+
+            let total = await Transaction.aggregate([
+                {$match : criteria},
+                {
+                    $group : {
+                        _id: null,
+                        value: {
+                            $sum: "$value"
+                        }
+                    }
+                }
+            ])
+
+            res.status(200).json({
+                data: history,
+                total: total.length ? total[0].value : 0
+            })
+        } catch (error) {
+            res.status(500).json({message: error.message || 'Internal Server Error!'});
+        }
+    },
+
+    historyDetail: async (req, res) => {
+        try {
+            const {id} = req.params;
+            const history =  await Transaction.findOne({_id: id})
+            
+            if(!history) return res.status(404).json({message: 'Product Not Found'});
+
+            res.status(200).json({
+                data: history
+            })
         } catch (error) {
             res.status(500).json({message: error.message || 'Internal Server Error!'});
         }
--- app/player/router.js
+++ app/player/router.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var router = express.Router();
-const { landingPage, detailPage, category } = require("./controller");
+const { landingPage, detailPage, category, checkout, history, historyDetail } = require("./controller");
+const {isLoginPlayer} = require('../middleware/auth');
 
 /* GET home page. */
 router.get("/landing-page", landingPage);
 router.get("/detail/:id", detailPage);
 router.get("/category", category);
+router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
 
 module.exports = router;
